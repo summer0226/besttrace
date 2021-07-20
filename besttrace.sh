ecfae9a4ea89928403b4ecf2ec66537dce67aa59
summer0226 <liu79830956@qq.com>
2021-07-20 17:39:19 +0800

    Update besttrace.sh

--- besttrace.sh
+++ besttrace.sh
@@ -52,7 +52,7 @@ root() {
 }
 
 xiazai(){
-    wget https://raw.githubusercontent.com/summer0226/besttrace/main/besttrace4linux.zip
+    wget https://cdn.ipip.net/17mon/besttrace4linux.zip
     unzip besttrace4linux.zip
     chmod +x besttrace
     echo -e "${OK} ${GreenBG} 安装完成 ${Font}"
